797c33c3eeedbd3912d230faf1ea3bb5fc4d2fb7
Augusto Carminati <augusto_carminati04@hotmail.com>
2023-08-26 14:10:38 -0300

    se continuo trabajando sobre la preentrega

--- pre-entrega/src/components/ItemListContainer/ItemList.jsx
+++ pre-entrega/src/components/ItemListContainer/ItemList.jsx
@@ -1,19 +1,36 @@
 import React from 'react';
+import propTypes from "prop-types";
 
-const ItemList = ({products}) => {
-    return <div>
-        <h2>ItemList</h2>
+const ItemList = ({products, isLoading}) => {
+    return(
+        <div>
+            <h2>ItemList</h2>
 
-        <ul>
-            {products.map((product) =>(
-                <li key={product.id}>
-                <h3>{product.name}</h3>
-                <p>${product.price}</p>
-                <p>{product.category}</p>
-                </li>
-            ))}
-        </ul>
-    </div>;
-    };
+            {isLoading && <p>Loading</p>}
+
+            {!isLoading && products.length === 0 && <p>No products</p>}
+
+            <div className="row">
+                {products.map((product) =>(
+                    <div className="col-6 col-md-4 col-lg-3" key={product.id}>
+                        <div className="card box-shadow rounded mb-4">
+                            <img src="" alt="" className="card-img-top rounded"/>
+                            <div className="card-body">
+                                <h5 className="card-title">{product.name}</h5>
+                                <h5 className="card-title">{product.price}</h5>
+                                <h5 className="card-title">{product.category}</h5>
+                                <h5 className="card-title">{product.stock}</h5>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>;
+        </div>
+    );
+};
+
+ItemList.propTypes = {
+    products: propTypes.array.isRequired,
+};
 
 export default ItemList;
---
+++ pre-entrega/src/services.js
@@ -0,0 +1,37 @@
+const products= [
+    {id: 1, name: "remera", description: "una remera", stock: 10, category: "shirts", price: 10000},
+    {id: 2, name: "remera", description: "una remera", stock: 9, category: "shirts", price: 10000}, 
+    {id: 3, name: "buzo", description: "un buzo", stock: 10, category: "coverall", price: 20000},
+    {id: 4, name: "buzo", description: "un buzo", stock: 9, category: "coverall", price: 20000},
+    {id: 5, name: "campera", description: "una campera", stock: 10, category: "jacket", price: 35000},
+    {id: 6, name: "campera", description: "una campera", stock: 9, category: "jacket", price: 35000},
+    {id: 7, name: "conjunto", description: "un conjunto", stock: 10, category: "set", price: 45000},
+    {id: 8, name: "conjunto", description: "un conjunto", stock: 9, category: "set", price: 45000},
+];
+
+export const getProduct = (id) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const product = products.find(p => p.id == id)
+
+            if (product){
+                resolve(product)
+            } else {
+                reject("No existe el producto");
+            }
+
+    }, 1000);
+    });
+};
+
+export const getProducts = (category) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const productsFiltered = category ? products.filter(product => product.category == category) : products;
+
+            resolve(productsFiltered);
+
+        }, 1000);
+    });
+
+};
